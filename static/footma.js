0dc712467c37a73a98989cfa25c64509f8741f7c
ok0749 <63099945+ok0749@users.noreply.github.com>
2020-11-06 15:09:03 +0900

    date select

--- static/footma.js
+++ static/footma.js
@@ -1,8 +1,9 @@
-const select = document.querySelector('select')
+const modeSelect = document.querySelector('#mode-select')
+const dateSelect = document.querySelector('#date-select')
 const ul = document.querySelector('ul')
 const body = document.querySelector('body')
 
-
+// 화면에 나와있는 목록 제거
 const removeLiElement = () => {
     if (ul.hasChildNodes()) {
         const li = document.querySelectorAll('.content')
@@ -12,6 +13,7 @@ const removeLiElement = () => {
     }
 }
 
+// 전체글 불러오기
 const handleTotal = () => {
     removeLiElement();
     axios.get('/total').then((total) => {
@@ -20,8 +22,12 @@ const handleTotal = () => {
             li.innerHTML = element.content
             li.className = "content"
             ul.appendChild(li)
-
         });
+
+
+
+
+
     }).catch((error) => {
         console.log(error)
     })
@@ -29,7 +35,9 @@ const handleTotal = () => {
 
 let modeElement = [];
 
+// 모드 선택시 해당 글 불러오기
 const handleMode = (mode) => {
+    dateSelect.removeAttribute('disabled')
     removeLiElement()
     axios.get('/total').then((total) => {
         total.data.forEach(element => {
@@ -44,34 +52,89 @@ const handleMode = (mode) => {
                 li.className = "content"
                 ul.appendChild(li)
             }
-
         });
-
+        dateSelect.addEventListener('change', handleDate)
     }).catch((error) => {
         console.log(error)
     })
 }
 
-
-
-
 const handleChange = () => {
     let mode = ''
     modeElement.splice(0, modeElement.length)
-    if (select.value === 'total') {
+    if (modeSelect.value === 'total') {
+        dateSelect.disabled = "disabled"
         handleTotal()
-    } else if (select.value === 'player') {
+    } else if (modeSelect.value === 'player') {
         mode = '용병구함'
         handleMode(mode)
-    } else if (select.value === 'match') {
+        // dateSelect.addEventListener('change', handleDate)
+    } else if (modeSelect.value === 'match') {
         mode = '매치'
         handleMode(mode)
+        // dateSelect.addEventListener('change', handleDate)
+    }
+}
+
+
+// 날짜 오름차순, 내림차순 정리 
+const handleDate = () => {
+    const li = document.querySelectorAll('li')
+    let date = []
+    li.forEach(element => {
+        const reg = /\d{1,2}월\d{1,2}일|\d{1,2}월\s\d{1,2}일|\d{1,2}[/]\d{1,2}일|\d{1,2}\s[/]\s\d{1,2}일 /g;
+        const result = reg.exec(element.innerHTML)
+        if (result !== null) {
+            const regex1 = /(\d{1,2})월(\d{1})일/g // 11월1일
+            const regex11 = /(\d{1,2})월(\d{2})일/g // 11월01일, 11월11일
+            const regex2 = /(\d{1,2})월\s(\d{1})일/g // 11월 1일
+            const regex21 = /(\d{1,2})월\s(\d{2})일/g // 11월 01일, 11월 11일
+            const regex3 = /(\d{1,2})[/](\d{1})일/g // 11/1
+            const regex31 = /(\d{1,2})[/](\d{2})일/g // 11/01, 11/11
+            const regex4 = /(\d{1,2})\s[/]\s(\d{1})일/g // 11 / 1,   
+            const regex41 = /(\d{1,2})\s[/]\s(\d{2})일/g // 11 / 01, 11 / 11  
+
+            result[0] = result[0].replace(regex1, "$10$2")
+            result[0] = result[0].replace(regex11, "$1$2")
+            result[0] = result[0].replace(regex2, "$10$2")
+            result[0] = result[0].replace(regex21, "$1$2")
+            result[0] = result[0].replace(regex3, "$10$2")
+            result[0] = result[0].replace(regex31, "$1$2")
+            result[0] = result[0].replace(regex4, "$10$2")
+            result[0] = result[0].replace(regex41, "$1$2")
+            date.push(result)
+            // console.log(result)
+        }
+    });
+    date.sort()
+    if (dateSelect.value === 'up') {
+        removeLiElement();
+        date.forEach(element => {
+            const li = document.createElement('li')
+            li.innerHTML = element['input']
+            li.className = "content"
+            ul.appendChild(li)
+            // console.log(element['input'])
+        });
+    } else if (dateSelect.value === 'down') {
+        removeLiElement();
+        date.reverse()
+        date.forEach(element => {
+            const li = document.createElement('li')
+            li.innerHTML = element['input']
+            li.className = "content"
+            ul.appendChild(li)
+            // console.log(element['input'])
+        });
+    } else if (dateSelect.value === 'default') {
+        handleTotal();
     }
 }
 
 const init = () => {
+    dateSelect.disabled = "disabled"
     handleTotal();
-    select.addEventListener('change', handleChange)
+    modeSelect.addEventListener('change', handleChange)
 }
 
 init()
